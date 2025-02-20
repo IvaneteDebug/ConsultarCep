51c5460b956c64bb2f3d279b597594ba14bad7de
Ivanete <ivanetevieira1000@gmail.com>
2025-02-20 19:58:50 -0300

    Implementação do serviço de consulta de CEP retornando os dados.

---
+++ ConsultarCep.API/Services/CepService.cs
@@ -0,0 +1,64 @@
+﻿using ConsultarCep.API.DTOs;
+using ConsultarCep.API.Handlers;
+using ConsultarCep.API.Https;
+using ConsultarCep.API.Models;
+using ConsultarCep.API.Repositories;
+
+namespace ConsultarCep.API.Services
+{
+    public class CepService : ICepService
+    {
+        private readonly ICepHttpClient _viaCepHttpClient;
+        private readonly IConsultarCepRepository _repository;
+
+        public CepService(ICepHttpClient viaCepHttpClient, IConsultarCepRepository repository)
+        {
+            _viaCepHttpClient = viaCepHttpClient;
+            _repository = repository;
+        }
+
+        public async Task<CepResponseDTO?> ConsultarCep(string cep)
+        {
+            var cepExistente = await _repository.ObterCepAsync(cep);
+            if (cepExistente != null)
+            {
+                throw new ConsultaCepException.CepAlreadyExistsException(
+                    "CEP já cadastrado.",
+                    cepExistente.Cep!
+                );
+            }
+
+            var response = await _viaCepHttpClient.GetAddressByCepAsync(cep);
+            if (response == null || response.Erro)
+            {
+                throw new ConsultaCepException.CepNotFoundException(
+                    "CEP não encontrado ou erro na consulta.",
+                    cep
+                );
+            }
+
+            var cepConsulta = new ConsultaCep
+            {
+                Cep = response.Cep,
+                Logradouro = response.Logradouro,
+                Bairro = response.Bairro,
+                Cidade = response.Localidade,
+                Estado = response.Uf,
+                Fonte = "ViaCep",
+                DataConsulta = DateTime.Now
+            };
+
+            await _repository.SalvarCepAsync(cepConsulta);
+
+            return new CepResponseDTO
+            {
+                Cep = response.Cep,
+                Logradouro = response.Logradouro,
+                Bairro = response.Bairro,
+                Localidade = response.Localidade,
+                Uf = response.Uf,
+                Fonte = "ViaCep"
+            };
+        }
+    }
+}
---
+++ ConsultarCep.API/Services/ICepService.cs
@@ -0,0 +1,9 @@
+﻿using ConsultarCep.API.DTOs;
+
+namespace ConsultarCep.API.Services
+{
+    public interface ICepService
+    {
+        Task<CepResponseDTO?> ConsultarCep(string cep);
+    }
+}
