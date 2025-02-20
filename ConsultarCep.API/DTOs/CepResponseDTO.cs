f013eaf0dd6ee0ba83a3ab0146e9bfd6558ff01c
Ivanete <ivanetevieira1000@gmail.com>
2025-02-20 20:09:07 -0300

    Adicionando DTOs para a comunicação e estrutura de dados entre camadas

---
+++ ConsultarCep.API/DTOs/CepResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsultarCep.API.DTOs
+{
+    public class CepResponseDTO
+    {
+        public string? Cep { get; set; }
+        public string? Logradouro { get; set; }
+        public string? Bairro { get; set; }
+        public string? Localidade { get; set; }
+        public string? Uf { get; set; }
+        public string? Fonte { get; set; }
+        public bool Erro { get; set; }
+
+        public CepResponseDTO() { }
+    }
+}
+
