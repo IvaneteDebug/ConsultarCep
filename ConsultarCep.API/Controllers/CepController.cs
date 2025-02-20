9c81c5e98d8580858e8f999783487d2f22226319
Ivanete <ivanetevieira1000@gmail.com>
2025-02-20 16:35:35 -0300

    Adicionando o CepController na estrutura do projeto

---
+++ ConsultarCep.API/Controllers/CepController.cs
@@ -0,0 +1,25 @@
+﻿using ConsultarCep.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsultarCep.API.Controllers
+{
+    [Route("api/cep")]
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+        private readonly ICepService _cepService;
+
+        public CepController(ICepService cepService)
+        {
+            _cepService = cepService;
+        }
+
+        [HttpGet("{cep}")]
+        public async Task<IActionResult> ConsultarCep(string cep)
+        {
+            var result = await _cepService.ConsultarCep(cep);
+            return Ok(result);
+        }
+    }
+}
+
