9e76825e34ebf2b8f5fedd865cbdcda22ea530d0
Ivanete <ivanetevieira1000@gmail.com>
2025-02-20 16:41:09 -0300

    Adicionando a classe do contexto CepDbContext

---
+++ ConsultarCep.API/Persistence/CepDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ConsultarCep.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsultarCep.API.Persistence
+{
+    public class CepDbContext : DbContext
+    {
+        public CepDbContext(DbContextOptions<CepDbContext> options) : base(options) { }
+
+        public DbSet<ConsultaCep> CepsConsultados { get; set; }
+    }
+}
+
