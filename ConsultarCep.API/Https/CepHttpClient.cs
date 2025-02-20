13095fdfa60979ffffccaa5d050d25d17754ef7f
Ivanete <ivanetevieira1000@gmail.com>
2025-02-20 20:05:04 -0300

    Adicionando configuração da comunicação da API, garantindo maior segurança nas requisições

---
+++ ConsultarCep.API/Https/CepHttpClient.cs
@@ -0,0 +1,26 @@
+﻿using ConsultarCep.API.DTOs;
+using System.Text.Json;
+
+namespace ConsultarCep.API.Https
+{
+    public class CepHttpClient : ICepHttpClient
+    {
+        private readonly HttpClient _httpClient;
+        private const string ViaCepUrl = "https://viacep.com.br/ws/{0}/json/";
+
+        public CepHttpClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<CepResponseDTO?> GetAddressByCepAsync(string cep)
+        {
+            var response = await _httpClient.GetAsync(string.Format(ViaCepUrl, cep));
+            if (!response.IsSuccessStatusCode)
+                return null;
+
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<CepResponseDTO>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
---
+++ ConsultarCep.API/Https/ICepHttpClient.cs
@@ -0,0 +1,9 @@
+﻿using ConsultarCep.API.DTOs;
+
+namespace ConsultarCep.API.Https
+{
+    public interface ICepHttpClient
+    {
+        Task<CepResponseDTO?> GetAddressByCepAsync(string cep);
+    }
+}
