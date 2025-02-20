e862c6b2549b8053bb9f31c6f9824c7c822feaee
Ivanete <ivanetevieira1000@gmail.com>
2025-02-20 19:56:59 -0300

    Alteração na estrutura do banco de dados e criação da migração.

---
+++ ConsultarCep.API/Persistence/ConsultaCepDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ConsultarCep.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsultarCep.API.Persistence
+{
+    public class ConsultaCepDbContext : DbContext
+    {
+        public ConsultaCepDbContext(DbContextOptions<ConsultaCepDbContext> options) : base(options) { }
+
+        public DbSet<ConsultaCep> CepsConsultados { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ConsultaCep>(e =>
+            {
+                e.HasKey(e => e.Id);
+            });
+        }
+    }
+}
+
