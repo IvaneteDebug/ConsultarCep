2aae4e4dde86299a36ff21b8fa2ba95ad2c38fc7
Ivanete <ivanetevieira1000@gmail.com>
2025-02-20 19:50:24 -0300

    Refatoração do controlador  para melhorar a lógica de consulta de CEP

--- ConsultarCep.API/Controllers/CepController.cs
+++ ConsultarCep.API/Controllers/CepController.cs
@@ -8,17 +8,18 @@ namespace ConsultarCep.API.Controllers
     public class CepController : ControllerBase
     {
         private readonly ICepService _cepService;
-
         public CepController(ICepService cepService)
         {
             _cepService = cepService;
         }
 
+        // GET api/cep/1 Consultar CEP
         [HttpGet("{cep}")]
         public async Task<IActionResult> ConsultarCep(string cep)
         {
-            var result = await _cepService.ConsultarCep(cep);
-            return Ok(result);
+            var cepExistente = await _cepService.ConsultarCep(cep);
+
+            return Ok(new { message = "CEP consultado com sucesso.", data = cepExistente });
         }
     }
 }
