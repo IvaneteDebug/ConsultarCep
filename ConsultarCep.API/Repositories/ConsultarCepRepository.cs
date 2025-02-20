f6b6d1f64bb0a0a3398a084fc282824e8f6c8468
Ivanete <ivanetevieira1000@gmail.com>
2025-02-20 20:01:20 -0300

    Implementação do repositório para persistência de dados de CEP

---
+++ ConsultarCep.API/Repositories/ConsultarCepRepository.cs
@@ -0,0 +1,34 @@
+﻿using ConsultarCep.API.Handlers;
+using ConsultarCep.API.Models;
+using ConsultarCep.API.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsultarCep.API.Repositories
+{
+    public class ConsultarCepRepository : IConsultarCepRepository
+    {
+        private readonly ConsultaCepDbContext _context;
+
+        public ConsultarCepRepository(ConsultaCepDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ConsultaCep?> ObterCepAsync(string cep)
+        {
+            return await _context.CepsConsultados.FirstOrDefaultAsync(c => c.Cep == cep);
+        }
+
+        public async Task SalvarCepAsync(ConsultaCep cep)
+        {
+            var cepExistente = await ObterCepAsync(cep.Cep!);
+            if (cepExistente != null)
+            {
+                throw new ConsultaCepException.CepAlreadyExistsException("CEP já cadastrado.", cep.Cep!);
+            }
+
+            _context.CepsConsultados.Add(cep);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ ConsultarCep.API/Repositories/IConsultarCepRepository.cs
@@ -0,0 +1,10 @@
+﻿using ConsultarCep.API.Models;
+
+namespace ConsultarCep.API.Repositories
+{
+    public interface IConsultarCepRepository
+    {
+        Task<ConsultaCep?> ObterCepAsync(string cep);
+        Task SalvarCepAsync(ConsultaCep cep);
+    }
+}
